bd6812f231498d5a5604b5db7cb9dd9cd89cabdb
Lramos <89459635+lramos373@users.noreply.github.com>
2021-09-11 09:47:21 -0300

    Modifica branch e Adiciona linha de comando livro
    Coloca o projeto em uma branch apenas, que a default do git.
    
    Cria novas linha de comando mostrando nome de livro e quantidades de paginas.

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -10,6 +10,8 @@ public class PrimeiroPrograma {
 
 
 
+        Livro livro1 = new Livro("o problema dos 3 corpos", 300);
+        System.out.println(livro1);
 
 
          /*   int a = 5;
@@ -17,5 +19,38 @@ public class PrimeiroPrograma {
         System.out.println("Hello World "+ (a+b));*/
 
 
+    }
+}
+class Livro {
+    private String nome;
+    private  Integer numPagina;
+
+    public Livro(String nome, Integer numPagina) {
+        this.nome = nome;
+        this.numPagina = numPagina;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPagina() {
+        return numPagina;
+    }
+
+    public void setNumPagina(Integer numPagina) {
+        this.numPagina = numPagina;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPagina=" + numPagina +
+                '}';
     }
 }
